4e1395b9e0edec42d9bf98d890f9b0069d620410
Miya096jp <miyawaki.yske@gmail.com>
2025-04-22 12:52:25 +0900

    blogs_helperのsanitized_content変数の名称をescaped_contentに変更

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,7 +2,7 @@
 
 module BlogsHelper
   def format_content(blog)
-    sanitized_content = h(blog.content)
-    simple_format(sanitized_content)
+    escaped_content = h(blog.content)
+    simple_format(escaped_content)
   end
 end
